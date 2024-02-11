8e1ef062cc0b2b73f43946dd75a7a903b154eced
Annelies <annelies2000@outlook.com>
2024-02-11 13:23:45 +0100

    bees greet eachother sometimes

--- dev/bees.ts
+++ dev/bees.ts
@@ -14,10 +14,13 @@ class Flower {
   private x: number;
   private y: number;
   private flowerEl: HTMLElement;
+  private color: number;
+  private colors: string[] = ['red', 'pink', 'blue', 'orange', 'purple'];
 
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, color: number) {
     this.x = x;
     this.y = y;
+    this.color = color;
 
     this.flowerEl = document.createElement("div")
     this.flowerEl.className = "flower"
@@ -28,6 +31,8 @@ class Flower {
 
     const flowerTop = document.createElement("div")
     flowerTop.className = "flower-top"
+
+    flowerTop.classList.add('--' + this.colors[this.color])
 
     this.flowerEl.appendChild(flowerTop)
     this.flowerEl.appendChild(stem)
@@ -58,29 +63,36 @@ class Flower {
 
   private updateFlowerPosition() {
     this.flowerEl.setAttribute("style", "top: " + this.y + "px; left: " + this.x + "px;")
+    
   }
 
 }
 
 class Bee {
+    private index: number;
     private x: number;
     private y: number;
     destination: {x: number, y: number} = {x: 0, y: 0}
     private el: HTMLElement;
     // receivedInstructions: {x: number, y: number};
+    isPollinating: boolean;
     isHoldingPollen: boolean;
     game: Game;
     flowerDistancePixels: number = 30
     beeDistancePixels: number = 30
+    private speechBubbleEl: HTMLElement
+    private isCommunicating: boolean = false;
 
-
-    constructor(startX: number, startY: number) {
+    constructor(startX: number, startY: number, index: number) {
         this.setNewDestination()
         this.x = startX;
         this.y = startY
+        this.index = index;
 
         this.el = document.createElement('div')
         this.el.className = "bee"
+        // this.el.innerText = this.index + ''
+
         const body = document.querySelector('body')
 
         const torso = document.createElement('div')
@@ -106,8 +118,12 @@ class Bee {
         antenna1.className = "bee-antenna"
         antenna2.className = "bee-antenna"
 
+        this.speechBubbleEl = document.createElement("div")
+        this.speechBubbleEl.className = "bee-speech --invisible"
+
         head.appendChild(antenna1)
         head.appendChild(antenna2)
+        head.appendChild(this.speechBubbleEl)
 
 
         this.el.append(torso)
@@ -140,7 +156,7 @@ class Bee {
       if(this.x < this.destination.x) {
           this.x++
           this.el.classList.add('--mirrored')
-        } else if(this.x > this.destination.x) {
+      } else if(this.x > this.destination.x) {
           this.el.classList.remove('--mirrored')
           this.x--
       }
@@ -159,8 +175,40 @@ class Bee {
     checkFlowers() {
       this.game.flowers.forEach(flower => {
         // if bee is near flower and flower not occupied, go to flower, get nectar
-        if(this.x + this.flowerDistancePixels > flower.getX() && this.x + this.flowerDistancePixels < flower.getX() + flower.getFlowerEl().getBoundingClientRect().width ) {
-          if(this.y + this.flowerDistancePixels > flower.getY() && this.y + this.flowerDistancePixels < flower.getY() + flower.getFlowerEl().getBoundingClientRect().height ) {
+        // if(this.x + this.flowerDistancePixels > flower.getX() && this.x + this.flowerDistancePixels < flower.getX() + flower.getFlowerEl().getBoundingClientRect().width ) {
+        //   if(this.y + this.flowerDistancePixels > flower.getY() && this.y + this.flowerDistancePixels < flower.getY() + flower.getFlowerEl().getBoundingClientRect().height ) {
+          if(
+            // this         |
+            //             other
+            (this.x + this.flowerDistancePixels > flower.getX() && this.x + this.flowerDistancePixels < flower.getX() + flower.getFlowerEl().getBoundingClientRect().width)
+            ||
+  
+            //            this
+            //    other           |
+            (this.x < flower.getX() + flower.getFlowerEl().getBoundingClientRect().width + this.flowerDistancePixels && this.x > flower.getX())
+            ||
+  
+            //      |         this
+            //    other
+            (this.x - this.flowerDistancePixels > flower.getX() && this.x - this.flowerDistancePixels < flower.getX() + flower.getFlowerEl().getBoundingClientRect().width)
+            ) {
+              
+            if(
+                // this         |
+                //             other
+                (this.y + this.flowerDistancePixels > flower.getY() && this.y + this.flowerDistancePixels < flower.getY() + flower.getFlowerEl().getBoundingClientRect().height)
+                ||
+  
+                //            this
+                //    other           |
+                (this.y < flower.getY() + flower.getFlowerEl().getBoundingClientRect().height + this.flowerDistancePixels && this.y > flower.getY())
+                ||
+  
+                //      |         this
+                //    other
+                (this.y - this.flowerDistancePixels > flower.getY() && this.y - this.flowerDistancePixels < flower.getY() + flower.getFlowerEl().getBoundingClientRect().height)
+              // this.y + this.beeDistancePixels > bee.getY() && this.y + this.beeDistancePixels < bee.getY() + bee.getEl().getBoundingClientRect().height 
+            ) {
             if(!flower.getIsOccupied() && !this.isHoldingPollen) {
               this.pollinate(flower);
             }
@@ -172,10 +220,52 @@ class Bee {
 
     checkFellowBees() {
       this.game.bees.forEach(bee => {
-        if(this.x + this.beeDistancePixels > bee.getX() && this.x + this.flowerDistancePixels < bee.getX() + bee.getEl().getBoundingClientRect().width ) {
-          if(this.y + this.beeDistancePixels > bee.getY() && this.y + this.beeDistancePixels < bee.getY() + bee.getEl().getBoundingClientRect().height ) {
+        if(
+          // this         |
+          //             other
+          (this.x + this.beeDistancePixels > bee.getX() && this.x + this.beeDistancePixels < bee.getX() + bee.getEl().getBoundingClientRect().width)
+          ||
+
+          //            this
+          //    other           |
+          (this.x < bee.getX() + bee.getEl().getBoundingClientRect().width + this.beeDistancePixels && this.x > bee.getX())
+          ||
+
+          //      |         this
+          //    other
+          (this.x - this.beeDistancePixels > bee.getX() && this.x - this.beeDistancePixels < bee.getX() + bee.getEl().getBoundingClientRect().width)
+          ) {
+            
+          if(
+              // this         |
+              //             other
+              (this.y + this.beeDistancePixels > bee.getY() && this.y + this.beeDistancePixels < bee.getY() + bee.getEl().getBoundingClientRect().height)
+              ||
+
+              //            this
+              //    other           |
+              (this.y < bee.getY() + bee.getEl().getBoundingClientRect().height + this.beeDistancePixels && this.y > bee.getY())
+              ||
+
+              //      |         this
+              //    other
+              (this.y - this.beeDistancePixels > bee.getY() && this.y - this.beeDistancePixels < bee.getY() + bee.getEl().getBoundingClientRect().height)
+          ) {
+            
+            // normal meeting, outside
+            if(Math.round(Math.random() * 300) == 1 && !this.isCommunicating && !this.isPollinating) {
+              // greet other bee!
+              this.showSpeechBubble(":)")
+              bee.showSpeechBubble(":D")
+              setTimeout(() => {
+                this.hideSpeechBubble()
+                bee.hideSpeechBubble()
+              }, 1000)
+            }
+
             if(this.isHoldingPollen) {
               // if in hive, dance
+
             }
           }
         }
@@ -189,24 +279,42 @@ class Bee {
     }
 
     pollinate(flower: Flower) {
+      this.hideSpeechBubble();
+      this.isPollinating = true;
       this.destination = {x: flower.getX(), y: flower.getY()}
       flower.setIsOccupied(true)
       setTimeout(() => {
         this.togglePollen()
         this.setNewDestination(null, 0)
         flower.setIsOccupied(false)
+        this.isPollinating = false;
       }, 2000);
     }
 
     togglePollen() {
       if(!this.isHoldingPollen) {
         this.isHoldingPollen = true;
-        this.el.querySelector('.pollen').classList.remove('--invisible')
+        if(this.el.querySelector('.pollen')) {
+          this.el.querySelector('.pollen').classList.remove('--invisible')
+        }
       } else {
         this.isHoldingPollen = false;
-        this.el.querySelector('.pollen').classList.add('--invisible')
+        if(this.el.querySelector('.pollen')) {
+          this.el.querySelector('.pollen').classList.add('--invisible')
+        }
         this.game.increaseScore()
       }
+    }
+
+    showSpeechBubble(text: string) {
+      this.isCommunicating = true;
+      this.speechBubbleEl.innerText = text ? text : ''
+      this.speechBubbleEl.classList.remove('--invisible')
+    }
+    
+    hideSpeechBubble() {
+      this.isCommunicating = false;
+      this.speechBubbleEl.classList.add('--invisible')
     }
 
     dance() {
@@ -224,6 +332,10 @@ class Bee {
     getEl() {
       return this.el
     }
+
+    getIndex() {
+      return this.index
+    }
 }
 
 class Game {
@@ -239,15 +351,15 @@ class Game {
     this.hive = new Hive()
     this.score = 0;
 
-    for(let i = 0; i < 70; i++) {
+    for(let i = 0; i < 30; i++) {
       const flowerAreaY = 200 + (Math.random() * (window.innerHeight - 200))
       const flowerAreaX = Math.random() * (window.innerWidth)
-      this.flowers.push(new Flower(flowerAreaX, flowerAreaY))
+      this.flowers.push(new Flower(flowerAreaX, flowerAreaY, Math.round(Math.random() * 5)))
     }
     
-    for(let i = 0; i < 30; i++) {
+    for(let i = 0; i < 10; i++) {
       // this.bees.push(new Bee(Math.random() * (window.innerWidth),0))
-      this.bees.push(new Bee(0,0))
+      this.bees.push(new Bee(0,0, i))
     }    
 
     this.scoreEl = document.createElement("div")
